8a10a31d824e432d20a3fc71958ae29b586c3c6b
Robbie-Vavrik <rvavrik@millikin.edu>
2025-03-12 14:30:06 -0500

    Outline for the project Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    // scan is used as a class variable (not global) for now to handle input
+    private static final Scanner scan = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+        int choice = 0;
+        while (choice < 1 || choice > 3) {
+            clearScreen();
+            System.out.println("""
+                    Welcome to our recipe book
+                    Please select which meal your eating:
+                    \t1. Breakfast
+                    \t2. Lunch
+                    \t3. Dinner
+                    """);
+
+            choice = scan.nextInt();
+            scan.nextLine(); // remove extra newline
+
+            if (choice >= 3 && choice > 1) {
+                System.out.print("Thanks for making a correct choice");
+                break;
+            }
+        }
+
+        switch (choice) {
+            case 1:
+                breakfastRecipes();
+                break;
+            case 2:
+                lunchRecipes();
+                break;
+            case 3:
+                dinnerRecipes();
+                break;
+            default:
+                System.out.println("Invalid choice! Exiting...");
+                break;
         }
     }
+    private static void  breakfastRecipes(){
+
+    }
+
+    private static void  lunchRecipes(){
+
+    }
+
+    private static void  dinnerRecipes(){
+
+    }
+
+    private static void clearScreen() {
+        // System has a few cool methods including one that gets the OS name
+        if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+        } else {
+            // This works in Linux and ....Mac?
+            System.out.print("\033[H\033[2J");
+        }
+        // Always flush when you use escape codes or use too many newlines
+        System.out.flush();
+
+    }
 }
+
+
