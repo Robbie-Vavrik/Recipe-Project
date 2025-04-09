20dd3eac1362ee67fab94f8f3af78508dd46de08
Robbie-Vavrik <rvavrik@millikin.edu>
2025-04-09 14:51:27 -0500

    Update Main.java
    I made it so that we have different classes for the different meal times and an overall class Recipe

--- Main.java
+++ Main.java
@@ -1,59 +1,61 @@
 import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
     // scan is used as a class variable (not global) for now to handle input
     private static final Scanner scan = new Scanner(System.in);
 
-
+    /**
+     * A collection of Dice, Card, and other TableTop games.
+     * Completed as spikes in IS 221: Programming Fundamentals
+     * @param args Args are passed from the commandline (think flags)
+     */
     public static void main(String[] args) {
+        // Display main menu & get user choice (repeat until there is a correct choice)
 
-        int choice = 0;
-        while (choice < 1 || choice > 3) {
+        DinnerRecipes dinnerRecipes = new DinnerRecipes();
+        LunchRecipes lunchRecipes = new LunchRecipes();
+        BreakfastRecipes breakfastRecipes = new BreakfastRecipes();
+
+
+
+        // Using label to break out of game - should use a function & return
+        gameLoop:
+        while (true) {
             clearScreen();
             System.out.println("""
-                    Welcome to our recipe book
-                    Please select which meal your eating:
-                    \t1. Breakfast
-                    \t2. Lunch
-                    \t3. Dinner
+                    Welcome to our little Game System!
+                    Please select one of the following games:
+                    \t1. Breakfast Recipes 
+                    \t2. Lunch Recipes 
+                    \t3. Dinner Recipes 
+                    \t9. Exit
                     """);
 
-            choice = scan.nextInt();
-            scan.nextLine(); // remove extra newline
+            int choice = 0;
+            choice = Integer.parseInt(scan.nextLine());
+            // above removes extra newline & ensures an integer value
 
-            if (choice >= 3 && choice > 1) {
-                System.out.print("Thanks for making a correct choice");
-                break;
+            switch (choice) {
+                case 1:
+                    breakfastRecipes.playBreakfastRecipes();
+                    break;
+                case 2:
+                    lunchRecipes.playLunchRecipes();
+                    break;
+                case 3:
+                    dinnerRecipes.playDinnerRecipes();
+                    break;
+                case 9:
+                    System.out.println("Thanks for playing! Goodbye!");
+                    break gameLoop;
+                default:
+                    System.out.println("Invalid choice! Exiting...");
+                    break gameLoop;
             }
         }
-
-        switch (choice) {
-            case 1:
-                breakfastRecipes();
-                break;
-            case 2:
-                lunchRecipes();
-                break;
-            case 3:
-                dinnerRecipes();
-                break;
-            default:
-                System.out.println("Invalid choice! Exiting...");
-                break;
-        }
     }
-    private static void  breakfastRecipes(){
-
-    }
-
-    private static void  lunchRecipes(){
-
-    }
-
-    private static void  dinnerRecipes(){
-
-    }
-
     private static void clearScreen() {
         // System has a few cool methods including one that gets the OS name
         if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
